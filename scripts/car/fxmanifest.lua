7b02baefd5101404495d68a7bf07182b8b037f81
downloador <38085070+downloador@users.noreply.github.com>
2021-07-05 15:49:52 +0200

    Create fxmanifest.lua

---
+++ scripts/car/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author '@downloador at https://github.com/FiveM-Flux'
+description 'C# script for command: car'
+version '1.0.0'
+
+client_scripts {
+    'car.dll'
+}
